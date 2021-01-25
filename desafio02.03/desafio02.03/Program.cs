c4fd51aee104f96450e22a797a5130be0baa9a62
ximena815 <68305685+ximena815@users.noreply.github.com>
2021-01-25 11:21:39 -0500

    Add files via upload

---
+++ desafio02.03/desafio02.03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace desafio02._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entradas
+            Console.WriteLine("ingrese z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese cGrados: ");
+            double cGrados = double.Parse(Console.ReadLine());
+
+            //Proceso
+            double c = cGrados * (Math.PI / 180.0);
+
+            double t = (z / (Math.Sin(c)));
+
+            Console.WriteLine("t: " + t);
+
+            double y = Math.Sqrt(((t) * (t)) - ((z) * (z)));
+            Console.WriteLine("y: " + y);
+
+            double a = 180 - 90 - cGrados;
+            Console.WriteLine("a: " + a);
+        }
+    }
+}
